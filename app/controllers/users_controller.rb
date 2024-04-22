f0485f475c5bafae27afa116cc72d44f8856d548
Varenya Thyagaraj <varen90@gmail.com>
2024-04-22 22:59:32 +0530

    add error messages for invalid inputs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
-  def new
-  end
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_param)
+    if @user.save
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+    def user_param
+      params.require(:user).permit(:email, :password,:password_confirmation)
+    end
+
 end
