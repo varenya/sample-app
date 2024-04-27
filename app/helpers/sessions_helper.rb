36f2042d62bf1e4220f4fa92473c77ecce4b793c
Varenya Thyagaraj <varen90@gmail.com>
2024-04-27 13:00:00 +0530

    add hotwire stuff

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,18 @@
 module SessionsHelper
+  def login(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
+
+  def logged_in?
+    puts "current user"
+    puts current_user
+    !current_user.nil?
+  end
+
 end
