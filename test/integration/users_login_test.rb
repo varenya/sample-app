1589847fe2f54563d1b8240aacda4f1263d75c9d
Varenya Thyagaraj <varen90@gmail.com>
2024-04-30 09:56:31 +0530

    refactor tests into separate classes

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -4,42 +4,66 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
   end
+end
 
-  test "error flash works as expected" do
+class InvalidLoginTest < UsersLoginTest
+  test "login path" do
     get login_path
-    assert_template "sessions/new"
-    post login_path, params: { session: { email: "v@test.com", password: "alsjflsjdf" } }
-    assert_response :unprocessable_entity
-    assert_select "div.alert-danger"
-    get root_path
-    assert_select "div.alert-danger", :count => 0
+    assert_template 'sessions/new'
   end
 
-  test "login with valid information proceeded by logout" do
-    post login_path, params: { session: { email: @user.email, password: 'password' } }
-    assert is_logged_in?
-    assert_redirected_to @user
-    follow_redirect!
-    assert_template 'users/show'
-    assert_select "a[href=?]", login_path, count: 0
-    assert_select "a[href=?]", logout_path, count: 1
-    assert_select "a[href=?]", user_path(@user)
-
-    delete logout_path
-    assert_response :see_other
-    assert_redirected_to root_path
+  test "login with valid username and password" do
+    post login_path, params: { session: { email: @user.email, password: 'invalid' } }
     assert_not is_logged_in?
-    follow_redirect!
-    assert_template root_path
-    assert_select "a[href=?]", login_path
-    assert_select "a[href=?]", logout_path, count: 0
-    assert_select "a[href=?]", user_path(@user), count: 0
-  end
-
-  test "login with invalid information" do
-    post login_path, params: { session: { email: @user.email, password: 'foobar' } }
-    assert_response :unprocessable_entity
-    assert_template "sessions/new"
-    assert_not flash.nil?
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
   end
 end
+
+class ValidLogin < UsersLoginTest
+  def setup
+    super
+    post login_path params: { session: { email: @user.email, password: 'password' } }
+  end
+end
+
+class ValidLoginTest < ValidLogin
+  test "valid login" do
+    assert is_logged_in?
+    assert_redirected_to @user
+  end
+
+  test "redirect after login" do
+    follow_redirect!
+    assert_template "users/show"
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", logout_path
+  end
+end
+
+class Logout < ValidLogin
+  def setup
+    super
+    delete logout_path
+  end
+end
+
+class LogoutTest < Logout
+  test "successful logout" do
+    assert_not is_logged_in?
+    assert_response :see_other
+    assert_redirected_to root_url
+  end
+
+  test "redirect after logout" do
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", user_path(@user), count: 0
+    assert_select "a[href=?]", logout_path, count: 0
+  end
+end
+
+
